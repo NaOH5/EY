4c58ef7529a052c9733819ae75acba7bc7a2e63a
NaOH5 <kaya1678@outlook.com>
2024-10-21 18:51:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["⚖️ Glory to GB and MoJ", "⚖️ Keeping Peace, Building Future."];
+const statusMessages = ["⚖️ Glory to GB and MoJ", "⚖️ Keeping Peace, Building Future.",  "⚖️ Fighting the nabons", "⚖️ For a brighter future!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
